45feee9be6b7a1fb44624781fc590767d9e6037b
Le Thang <thnagbiet17@gmail.com>
2020-10-27 00:21:20 +0700

    Fix add function

--- add.js
+++ add.js
@@ -2,7 +2,6 @@ const { createMathOperation } = require("./internal/createMathOperation");
 
 const add = createMathOperation((augend, addend) => augend + addend, 0);
 
-let result = add( 1, 4);
-
-console.log('result:', result, typeof result);
-
+module.exports = {
+  add,
+};
